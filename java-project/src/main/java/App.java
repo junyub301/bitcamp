057bfb390e5b9fdff1e16e355b4943631378bc4d
junyub301 <junyub0301@gmail.com>
2017-09-25 17:24:04 +0900

    .

--- java-project/src/main/java/App.java
+++ java-project/src/main/java/App.java
@@ -1,19 +1,22 @@
+import java.util.Scanner;
+
 public class App {
   
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int i,j,k;
         System.out.print("이름: ");
         System.out.println("홍길동");
         System.out.print("국어: ");
-        System.out.println(100);
+        i = sc.nextInt();
         System.out.print("영어: ");
-        System.out.println(100);
+        j = sc.nextInt();
         System.out.print("영어: ");
-        System.out.println(100);
+        k = sc.nextInt();
         System.out.print("총점: ");
-        System.out.println(300);
+        System.out.println(i+j+k);
         System.out.print("평균: ");
-        System.out.println(100.0);
+        System.out.println((double)((i+j+k)/3));
 
 
     }
