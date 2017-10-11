5e89729a806a4383807b5c6b8f180ff9b2025f19
junyub301 <junyub0301@gmail.com>
2017-10-11 10:26:51 +0900

    ..

--- java-basic/src/main/java/bitcamp/java100/Test15_8.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_8.java
@@ -32,14 +32,20 @@ public class Test15_8 {
 
         boolean b1 = false;
         boolean result = false && (b1 = true);
-
-        System.out.println(true && (b1 = true));
         System.out.printf("b1 = %b, result = %b\n",b1, result);
-
+        
         b1 = false;
         result = false & (b1 = true);
         System.out.printf("b1 = %b, result = %b\n",b1, result);
         
+        b1 = false;
+        result = true || (b1 = true);
+        System.out.printf("b1 = %b, result = %b\n",b1, result);
+
+        b1 = false;
+        result = true | (b1 = true);
+        System.out.printf("b1 = %b, result = %b\n",b1, result); 
+
     }
 
 }
