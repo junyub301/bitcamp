9c7961d00de23dd78d46064f9746bfefb6da9682
junyub301 <junyub0301@gmail.com>
2017-10-18 20:54:16 +0900

    '.'

--- java-basic/src/main/java/bitcamp/java100/Test21_8.java
+++ java-basic/src/main/java/bitcamp/java100/Test21_8.java
@@ -35,16 +35,20 @@
 package bitcamp.java100;
 
 import java.io.Console;
-
-
+import java.util.ArrayList;
 
 public class Test21_8 {
 
-    static class Student {
+    static class Member {
         String name;
         String email;
         String phone;
-        
+
+        String list1() {
+
+            return String.format("%s\t%s %s",this.name, this.email, this.phone);
+        }    
+
     }
 
     public static void main(String[] args){
@@ -52,40 +56,48 @@ public class Test21_8 {
 
         Console console = System.console();
 
-
-
         if (console == null) {
             System.err.println("콘솔을 지원하지 않습니다.");
             System.exit(1); // JVM을 종료한다.
         }
 
-        Student s = new Student();
-        String[] cnt = new String[50];
-        String save;
-        String con;
+        Member m = new Member();
+        ArrayList<String> list = new ArrayList<>();
 
 
         while (true) {
-            s.name = console.readLine("이름? ");
-            s.email = console.readLine("이메일? ");
-            s.phone = console.readLine("전화? ");
-            save = console.readLine("저장하시겠습니까?(y/n) ");
-            if (save.equals("y"))
+            m.name = console.readLine("이름? ");
+            m.email = console.readLine("이메일? ");
+            m.phone = console.readLine("전화? ");
+            if (console.readLine("저장하시겠습니까?(y/n) ").equals("y"))
             {
                 System.out.println("저장하였습니다.");
-                
+                list.add(m.list1());
 
             }
+            else {
+                System.out.println("저장을 취소하였습니다.");
+            }
+            if (console.readLine("계속 하시겠습니까?(y/n) ").equals("y")) {
+                continue;
+            }
+            else {
+                System.out.println("--------------------------");
+                for (String n : list) {
+                    System.out.println(n);
+                }
+            }
 
+            break;
         }
-
-       
 
     }
 
-
-
 }
+
+
+
+
 
 
 
