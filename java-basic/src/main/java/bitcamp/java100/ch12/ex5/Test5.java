2a24096da17376fc05ef41a05f97a62773f494fb
junyub301 <junyub0301@gmail.com>
2017-11-07 16:54:22 +0900

    '.'

--- java-basic/src/main/java/bitcamp/java100/ch12/ex5/Test5.java
+++ java-basic/src/main/java/bitcamp/java100/ch12/ex5/Test5.java
@@ -1,9 +1,6 @@
 package bitcamp.java100.ch12.ex5;
 
-public interface Test5 extends A, E {
-    // A의 m1()을 상속받으면 리턴타입이 void
-    // E의 m1()을 상속받으면 리턴타입이 String
+public class Test5 implements A, E {
     
-    // 
 
 }
