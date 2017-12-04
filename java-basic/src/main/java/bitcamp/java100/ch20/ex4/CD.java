84405befc39c596a882da662218887a0f4b6da32
junyub301 <junyub0301@gmail.com>
2017-12-04 17:28:26 +0900

    '.'

---
+++ java-basic/src/main/java/bitcamp/java100/ch20/ex4/CD.java
@@ -0,0 +1,33 @@
+package bitcamp.java100.ch20.ex4;
+
+public class CD {
+    private String title;
+    private String Singer;
+    
+    public CD() {
+        System.out.println("CD()");
+    }
+
+    @Override
+    public String toString() {
+        return "CD [title=" + title + ", Singer=" + Singer + "]";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return Singer;
+    }
+
+    public void setSinger(String singer) {
+        Singer = singer;
+    }
+    
+    
+}
