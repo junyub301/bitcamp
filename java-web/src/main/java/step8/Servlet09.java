d040dfe60ea4861b0fc919054772d71985903b7f
junyub301 <junyub0301@gmail.com>
2017-12-11 18:11:46 +0900

    '.'

--- java-web/src/main/java/step8/Servlet09.java
+++ java-web/src/main/java/step8/Servlet09.java
@@ -3,12 +3,15 @@ package step8;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+
+
 
 @SuppressWarnings("serial")
 @WebServlet("/step8/Servlet09")
@@ -22,14 +25,25 @@ public class Servlet09 extends HttpServlet {
         String photo1Filename = null;
         String photo2Filename = null;
         
+        String savePath = req.getServletContext().getRealPath("sample");
+        
+        MultipartRequest multi = new MultipartRequest(req, savePath, "UTF-8");
+        name = multi.getParameter("name");
+        age = multi.getParameter("age");
+        photo1Filename = multi.getFilesystemName("photo1Filename");
+        photo2Filename = multi.getFilesystemName("photo2Filename");
+        
+        
         
         req.setCharacterEncoding("UTF-8");
         res.setContentType("text/plain;charset=UTF-8");
         PrintWriter out = res.getWriter();
         out.printf("이름 = %s\n", name);
         out.printf("나이 = %s\n", age);
-        out.printf("사진1 = %s\n", photo1Filename);
+        out.printf("사진1 = %s\n", photo1Filename); 
         out.printf("사진2 = %s\n", photo2Filename);
+        
+        
     }
 }
 
